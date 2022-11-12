3e7975bed24d2295ad10e322716965d9d205e543
prakharnagore <prakhar.nagore@accessai.co>
2022-11-13 00:00:43 +0530

    home ui frontend complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,20 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Profile from "./pages/profile";
+import LoggedInRoutes from "./routes/LoggedInRoutes";
+import NotLoggedInRoutes from "./routes/NotLoggedInRoutes";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/login" exact element={<Login />} />
-        <Route path="/profile" exact element={<Profile />} />
+        <Route element={<LoggedInRoutes />}>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/profile" exact element={<Profile />} />
+        </Route>
+        <Route element={<NotLoggedInRoutes />}>
+          <Route path="/login" exact element={<Login />} />
+        </Route>
       </Routes>
     </div>
   );
--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,7 +1,23 @@
-import React from "react";
+import { useRef, useState } from "react";
+import { useSelector } from "react-redux";
+import CreatePost from "../../components/createPost";
+import Header from "../../components/header";
+import LeftHome from "../../components/home/left";
+import RightHome from "../../components/home/right";
+import Stories from "../../components/home/stories";
+import "./style.css";
 
-const Home = () => {
-  return <div>Home</div>;
-};
-
-export default Home;
+export default function Home() {
+  const { user } = useSelector((user) => ({ ...user }));
+  return (
+    <div className="home">
+      <Header />
+      <LeftHome user={user} />
+      <div className="home_middle">
+        <Stories />
+        <CreatePost user={user} />
+      </div>
+      <RightHome user={user} />
+    </div>
+  );
+}
